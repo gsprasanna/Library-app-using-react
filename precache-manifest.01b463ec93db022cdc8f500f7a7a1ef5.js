2fd3063de1cc3208388e474bd4afdeb86ffc123f
Prasanna <prasanna@Prasannas-MacBook-Pro.local>
2019-10-13 18:30:12 +0530

    Updates

---
+++ precache-manifest.01b463ec93db022cdc8f500f7a7a1ef5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "948e17895c770890ef36e0c2790c19e0",
+    "url": "/Library-app-using-react/index.html"
+  },
+  {
+    "revision": "39530b459e32a315b9c3",
+    "url": "/Library-app-using-react/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "a4416f87cc8ede6e88c5",
+    "url": "/Library-app-using-react/static/css/main.5ea0f979.chunk.css"
+  },
+  {
+    "revision": "39530b459e32a315b9c3",
+    "url": "/Library-app-using-react/static/js/2.2059fabe.chunk.js"
+  },
+  {
+    "revision": "a4416f87cc8ede6e88c5",
+    "url": "/Library-app-using-react/static/js/main.33e92dc4.chunk.js"
+  },
+  {
+    "revision": "7a0d508936f77451f68a",
+    "url": "/Library-app-using-react/static/js/runtime-main.6e92c49c.js"
+  },
+  {
+    "revision": "81c29bc5316124abc2c07a92ebbc23e6",
+    "url": "/Library-app-using-react/static/media/loading.81c29bc5.gif"
+  }
+]);
